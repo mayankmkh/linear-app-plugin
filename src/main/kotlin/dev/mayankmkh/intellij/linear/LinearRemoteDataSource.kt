6bcbe05187ec00f4d560d19e1bb90ef03e3c466d
Mayank Kharbanda <mayank@invideo.io>
2021-04-05 13:48:42 +0530

    Pass teamId as function parameter instead of constructor parameter in LinearRemoteDataSource

--- src/main/kotlin/dev/mayankmkh/intellij/linear/LinearRemoteDataSource.kt
+++ src/main/kotlin/dev/mayankmkh/intellij/linear/LinearRemoteDataSource.kt
@@ -10,43 +10,44 @@ import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 import java.util.logging.Logger
 
-class LinearRemoteDataSource(private val teamId: String, private val apolloClient: ApolloClient) {
+class LinearRemoteDataSource(private val apolloClient: ApolloClient) {
 
-    fun getIssues(query: String?, offset: Int, limit: Int, withClosed: Boolean): Array<LinearTask> {
+    fun getIssues(teamId: String, query: String?, offset: Int, limit: Int, withClosed: Boolean): Array<LinearTask> {
         // FIXME: 04/04/21 Ignoring query and withClosed for now
         LOG.info("query: $query, offset: $offset, limit: $limit, withClosed: $withClosed")
-        return runBlocking { getIssues(offset, limit) }
+        return runBlocking { getIssues(teamId, offset, limit) }
     }
 
-    private suspend fun getIssues(offset: Int, limit: Int): Array<LinearTask> = withContext(Dispatchers.IO) {
-        var pageInfo = getIssuePageInfo(offset)
-        LOG.info("pageInfo: $pageInfo")
-
-        var remainingIssues = limit
-        val list: MutableList<IssuesQuery.Node> = ArrayList(limit)
-
-        while (remainingIssues > 0 && pageInfo.hasNextPage) {
-            LOG.info("remainingIssues: $remainingIssues")
-            val numberOfItems = remainingIssues.coerceAtMost(BATCH_SIZE)
-            val response =
-                apolloClient.query(IssuesQuery(teamId, numberOfItems, Input.optional(pageInfo.endCursor))).await()
-
-            val data = response.data ?: break
-            val issues = data.team.issues
-            val nodes = issues.nodes
-
-            list.addAll(nodes)
-            pageInfo = issues.pageInfo
-            remainingIssues -= nodes.size
+    private suspend fun getIssues(teamId: String, offset: Int, limit: Int): Array<LinearTask> =
+        withContext(Dispatchers.IO) {
+            var pageInfo = getIssuePageInfo(teamId, offset)
             LOG.info("pageInfo: $pageInfo")
+
+            var remainingIssues = limit
+            val list: MutableList<IssuesQuery.Node> = ArrayList(limit)
+
+            while (remainingIssues > 0 && pageInfo.hasNextPage) {
+                LOG.info("remainingIssues: $remainingIssues")
+                val numberOfItems = remainingIssues.coerceAtMost(BATCH_SIZE)
+                val response =
+                    apolloClient.query(IssuesQuery(teamId, numberOfItems, Input.optional(pageInfo.endCursor))).await()
+
+                val data = response.data ?: break
+                val issues = data.team.issues
+                val nodes = issues.nodes
+
+                list.addAll(nodes)
+                pageInfo = issues.pageInfo
+                remainingIssues -= nodes.size
+                LOG.info("pageInfo: $pageInfo")
+            }
+
+            LOG.info("list: " + list.joinToString { it.identifier })
+            list.map2Array { LinearTask(it) }
         }
 
-        LOG.info("list: " + list.joinToString { it.identifier })
-        list.map2Array { LinearTask(it) }
-    }
-
-    private suspend fun getIssuePageInfo(offset: Int): IssuesQuery.PageInfo {
-        val getPageInfo = getPageInfo(offset)
+    private suspend fun getIssuePageInfo(teamId: String, offset: Int): IssuesQuery.PageInfo {
+        val getPageInfo = getPageInfo(teamId, offset)
         return if (getPageInfo != null) {
             IssuesQuery.PageInfo(hasNextPage = getPageInfo.hasNextPage, endCursor = getPageInfo.endCursor)
         } else {
@@ -54,22 +55,23 @@ class LinearRemoteDataSource(private val teamId: String, private val apolloClien
         }
     }
 
-    private suspend fun getPageInfo(offset: Int): GetPageInfoQuery.PageInfo? = withContext(Dispatchers.IO) {
-        var pendingOffset = offset
-        val firstPageInfo = GetPageInfoQuery.PageInfo(hasNextPage = true, endCursor = null)
-        var pageInfo = firstPageInfo
+    private suspend fun getPageInfo(teamId: String, offset: Int): GetPageInfoQuery.PageInfo? =
+        withContext(Dispatchers.IO) {
+            var pendingOffset = offset
+            val firstPageInfo = GetPageInfoQuery.PageInfo(hasNextPage = true, endCursor = null)
+            var pageInfo = firstPageInfo
 
-        while (pendingOffset > 0 && pageInfo.hasNextPage) {
-            val pageOffset = pendingOffset.coerceAtMost(MAX_COUNT)
-            val getPageInfoQuery = GetPageInfoQuery(teamId, pageOffset, Input.optional(pageInfo.endCursor))
-            val response = apolloClient.query(getPageInfoQuery).await()
-            val data = response.data ?: break
-            pageInfo = data.team.issues.pageInfo
-            pendingOffset -= pageOffset
+            while (pendingOffset > 0 && pageInfo.hasNextPage) {
+                val pageOffset = pendingOffset.coerceAtMost(MAX_COUNT)
+                val getPageInfoQuery = GetPageInfoQuery(teamId, pageOffset, Input.optional(pageInfo.endCursor))
+                val response = apolloClient.query(getPageInfoQuery).await()
+                val data = response.data ?: break
+                pageInfo = data.team.issues.pageInfo
+                pendingOffset -= pageOffset
+            }
+
+            if (pageInfo === firstPageInfo) null else pageInfo
         }
-
-        if (pageInfo === firstPageInfo) null else pageInfo
-    }
 
     suspend fun testConnection(teamId: String) = withContext(Dispatchers.IO) {
         val response = apolloClient.query(TestConnectionQuery(teamId)).await()
